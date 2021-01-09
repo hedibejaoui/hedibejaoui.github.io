ec8aa8a9d7c15b42e5b99366f20c0500f7b75d71
Mohamed Hedi Bejaoui <mohamedhedi.bejaoui@infor.com>
2021-01-09 22:45:49 +0100

    Randomized the order of testimonials

--- js/custom.js
+++ js/custom.js
@@ -84,3 +84,35 @@ $(window).on("scroll", function () {
                 !1
             );
     });
+
+// This block is responsible for randomizing the owl-caroussel containing the testimonials
+var owl = $('.owl-carousel');
+(function($){
+
+    $.fn.shuffle = function() {
+
+        var allElems = this.get(),
+            getRandom = function(max) {
+                return Math.floor(Math.random() * max);
+            },
+            shuffled = $.map(allElems, function(){
+                var random = getRandom(allElems.length),
+                    randEl = $(allElems[random]).clone(true)[0];
+                allElems.splice(random, 1);
+                return randEl;
+            });
+
+        this.each(function(i){
+            $(this).replaceWith($(shuffled[i]));
+        });
+
+        return $(shuffled);
+
+    };
+
+})(jQuery);
+
+$(function () {
+    $(".owl-carousel .people_says").shuffle();
+owl.owlCarousel();
+    });
